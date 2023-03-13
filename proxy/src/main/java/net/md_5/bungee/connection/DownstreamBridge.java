20e2f3b929754aba1f8db28863b3266edf7059f7
ToeDev <73237279+ToeDev@users.noreply.github.com>
2023-03-13 14:54:55 -0400

    citizens team packets allowed through

--- proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
+++ proxy/src/main/java/net/md_5/bungee/connection/DownstreamBridge.java
@@ -262,7 +262,7 @@ public class DownstreamBridge extends PacketHandler
                 }
             }
         }
-        if ( !con.getServer().getInfo().getName().equalsIgnoreCase( "cv7wargames" ) && !team.getName().startsWith( "LD_" ) )
+        if ( !con.getServer().getInfo().getName().equalsIgnoreCase( "cv7wargames" ) && !team.getName().startsWith( "LD_" ) && !team.getName().startsWith( "CIT-" ) )
         {
             throw CancelSendSignal.INSTANCE;
         }
